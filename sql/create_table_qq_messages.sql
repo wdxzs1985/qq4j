866b6bd9e18a926f076f40f472840e732a8838ec
wangjue <wangjue@cnc-inc.co.jp>
2013-07-19 14:39:10 +0900

    sql script

--- sql/create_table_qq_messages.sql
+++ sql/create_table_qq_messages.sql
@@ -6,9 +6,9 @@ CREATE TABLE `qq_messages` (
   `owner` int(15) unsigned NOT NULL,
   `message` varchar(200) NOT NULL,
   `answer` varchar(200) NOT NULL,
-  `public` int(1) unsigned NOT NULL DEFAULT '0',
+  `public_flg` int(1) unsigned NOT NULL DEFAULT '0',
   `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `del_flg` int(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8$$
 
--- sql/create_table_qq_user.sql
+++ sql/create_table_qq_user.sql
@@ -1,12 +1,12 @@
 delimiter $$
 
 CREATE TABLE `qq_user` (
-  `account` int(10) unsigned NOT NULL,
-  `qq` int(10) unsigned NOT NULL,
-  `nickname` varchar(45) NOT NULL,
+  `account` int(15) unsigned NOT NULL,
+  `qq` int(15) unsigned NOT NULL,
   `faith` int(10) unsigned NOT NULL DEFAULT '0',
+  `black` int(1) unsigned NOT NULL DEFAULT '0',
   `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `del_flg` int(10) unsigned NOT NULL DEFAULT '0',
+  `del_flg` int(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`account`,`qq`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
