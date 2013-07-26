f32946fcbede632b6247242a87fa6ab715236771
wdxzs1985 <wdxzs1985@gmail.com>
2013-07-26 23:42:05 +0900

    swing登录界面

--- sql/create_schema.sql
+++
@@ -1,4 +0,0 @@
-delimiter $$
-
-CREATE DATABASE `qq4j` /*!40100 DEFAULT CHARACTER SET utf8 */$$
-
---
+++ swing/org/qq4j/swing/QQRobotMain.java
@@ -0,0 +1,61 @@
+package org.qq4j.swing;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.qq4j.core.QQContext;
+import org.qq4j.core.QQRobot;
+import org.qq4j.domain.QQUser;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ */
+public class QQRobotMain implements QQRobot {
+
+    public static void main(final String[] args) {
+        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/application-context.xml");
+        final QQRobot original = (QQRobot) context.getBean("default");
+        context.close();
+
+        final QQRobotMain app = new QQRobotMain(original);
+        app.startApplication();
+    }
+
+    private JFrame vFrame = null;
+    private JPanel vLoginPanel = null;
+    private QQRobot mRobot = null;
+
+    public void startApplication() {
+        this.vFrame = new JFrame();
+        this.vFrame.setSize(400, 300);
+        this.vFrame.setLocationRelativeTo(null);
+        this.vFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.vLoginPanel = new QQRobotLoginPanel(this);
+        this.vFrame.add(this.vLoginPanel);
+        this.vFrame.setVisible(true);
+        this.vFrame.invalidate();
+    }
+
+    QQRobotMain(final QQRobot robot) {
+        this.mRobot = robot;
+    }
+
+    @Override
+    public void startup(final QQUser user) {
+        this.mRobot.startup(user);
+        this.vFrame.setVisible(false);
+    }
+
+    @Override
+    public void shutdown() {
+        this.mRobot.shutdown();
+    }
+
+    @Override
+    public QQContext getContext() {
+        return this.mRobot.getContext();
+    }
+}
